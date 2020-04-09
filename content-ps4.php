da3ce4a9ec37fb59ee2d319ca001490cd66179bb
Jay Versluis <versluis2000@yahoo.com>
2020-04-09 18:49:06 -0400

    added Steam query page

---
+++ content-ps4.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+	<div class="inside-article">
+		<?php
+		/**
+		 * generate_before_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_featured_page_header_inside_single - 10
+		 */
+		do_action( 'generate_before_content' );
+
+		if ( generate_show_title() ) : ?>
+
+			<header class="entry-header">
+				<?php
+				/**
+				 * generate_before_page_title hook.
+				 *
+				 * @since 2.4
+				 */
+				do_action( 'generate_before_page_title' );
+
+				the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
+
+				/**
+				 * generate_after_page_title hook.
+				 *
+				 * @since 2.4
+				 */
+				do_action( 'generate_after_page_title' );
+				?>
+			</header><!-- .entry-header -->
+
+		<?php endif;
+
+		/**
+		 * generate_after_entry_header hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_post_image - 10
+		 */
+		do_action( 'generate_after_entry_header' );
+		?>
+
+		<div class="entry-content" itemprop="text">
+			<?php
+            the_content();
+            
+            // custom query goes here
+            // grab all games with platform
+            $query = new WP_Query( array( 
+                'platform' => 'ps4', 
+                'nopaging' => true )
+            );
+            $results = $query->found_posts;
+            echo "<p>There are <strong>$results PS4 Games</strong> in my collection:</p><div>";
+
+            // list all games
+            if ($query->have_posts() ) {
+                echo "";
+                while ($query->have_posts() ) {
+                    $query->the_post();
+                    echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+                }
+                echo "</div>";
+            } // end of games list
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
+
+		<?php
+		/**
+		 * generate_after_content hook.
+		 *
+		 * @since 0.1
+		 */
+		do_action( 'generate_after_content' );
+		?>
+	</div><!-- .inside-article -->
+</article><!-- #post-## -->

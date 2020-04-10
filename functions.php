4b9006060a9611cf56be36a052d0f0bef2d43b60
Jay Versluis <versluis2000@yahoo.com>
2020-04-10 18:28:44 -0400

    redefinded footer text

--- functions.php
+++ functions.php
@@ -113,3 +113,26 @@ add_filter( 'generate_category_list_output', function( $output ) {
   return $output;
   
 } );
+
+// redefine footer content
+remove_action( 'generate_credits', 'generate_add_footer_info' );
+
+if ( ! function_exists( 'generate_add_footer_info' ) ) {
+	add_action( 'generate_credits', 'generate_add_footer_info' );
+	/**
+	 * Add the copyright to the footer
+	 *
+	 * @since 0.1
+	 */
+	function generate_add_footer_info() {
+    $copyright = sprintf( '<span class="copyright">&copy; %1$s %2$s</span> &bull; %4$s <a href="%3$s" itemprop="url">%5$s</a> &bull; <a href="https://github.com/versluis/Games-Collection" target="_blank">Fork Me</a>',
+			date( 'Y' ),
+			'<a href="https://wpguru.tv">The WP Guru</a>',
+			esc_url( 'https://generatepress.com' ),
+			_x( 'Powered by', 'GeneratePress', 'generatepress' ),
+			__( 'GeneratePress', 'generatepress' )
+		);
+
+		echo apply_filters( 'generate_copyright', $copyright ); // WPCS: XSS ok.
+	}
+}
